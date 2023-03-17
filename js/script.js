862f83f2a718829e4be1d4e97aacf9e8002772e4
Lip-7 <zhighofficial@gmail.com>
2023-03-17 16:22:31 +0100

    minor fixes

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@ function play(e) {
     e.preventDefault();
     const playGround = document.getElementById('playground');
     playGround.innerHTML = '';
+    document.querySelector('h3').innerHTML = `Seleziona la difficoltà e premi play!`
     const level = document.getElementById('level').value; /* easy = 100 ,medium = 81, hard = 49 */
     let squareNumber;
     const nBombs = 16;
+    let userScore = 0
     switch(level){
         case 'easy':
             squareNumber = 100;
@@ -28,13 +30,18 @@ function play(e) {
         if (bombsArray.indexOf(i) >= 0) {
             square.addEventListener('click', function(){
                 square.classList.add('bomb');
+                const squares = document.getElementById('playground').querySelectorAll('div');
+                for (let j = 0; j < squares.length; j++){
+                    squares[j].removeEventListener('click')
+                }
             })
         }else{
             square.addEventListener('click', function(){
                 square.classList.add('safe');
+                userScore ++
+                document.querySelector('h3').innerHTML = `Your score is: ${userScore}`
             })
         }
-        console.log(square);
         playGround.appendChild(square);
     }
 }
